0e5db81ea17d917892a9a0352a0d1765f7ea46a3
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-10-26 14:21:31 +0200

    Created starting folder structure for backend part of the app (controllers, models, services and repositories packages), added dependencies (springboot devtools, spring data jpa, spring web and postrgeSQL driver)

---
+++ auction-app-backend/src/main/java/com/internship/auctionapp/AuctionAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internship.auctionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionAppBackendApplication.class, args);
+    }
+
+}
