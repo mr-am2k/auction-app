3b93f0dfc98adea739c39fccabfd3f2e6b2a2682
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-10-31 09:12:32 +0100

    Created structure for the navbar-footer task, and added some of the css variables in index.css file

---
+++ auction-app-ui/src/layouts/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import classes from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
---
+++ auction-app-ui/src/layouts/header/Header.tsx
@@ -0,0 +1,9 @@
+import classes from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
