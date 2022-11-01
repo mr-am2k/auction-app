9fe17b54c0277b51521debc81f63b1dae458966e
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-11-01 11:21:45 +0100

    Finished design for about us page

--- auction-app-ui/src/pages/about-us/AboutUs.tsx
+++ auction-app-ui/src/pages/about-us/AboutUs.tsx
@@ -4,45 +4,53 @@ const AboutUs = () => {
   return (
     <div className='c-about-us'>
       <h1>About Us</h1>
-      <div className='c-content'>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-          consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum
-          mauris, venenatis sit amet porttitor id, laoreet eu magna. In
-          convallis diam volutpat libero tincidunt semper. Ut aliquet erat
-          rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit amet
-          diam malesuada, eget laoreet quam molestie. In id elementum turpis.
-          Curabitur quis tincidunt mauris.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-          consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum
-          mauris, venenatis sit amet porttitor id, laoreet eu magna. In
-          convallis diam volutpat libero tincidunt semper. Ut aliquet erat
-          rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit amet
-          diam malesuada, eget laoreet quam molestie. In id elementum turpis.
-          Curabitur quis tincidunt mauris.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-          consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum
-          mauris, venenatis sit amet porttitor id, laoreet eu magna. In
-          convallis diam volutpat libero tincidunt semper. Ut aliquet erat
-          rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit amet
-          diam malesuada, eget laoreet quam molestie. In id elementum turpis.
-          Curabitur quis tincidunt mauris.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-          consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum
-          mauris, venenatis sit amet porttitor id, laoreet eu magna. In
-          convallis diam volutpat libero tincidunt semper. Ut aliquet erat
-          rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit amet
-          diam malesuada, eget laoreet quam molestie. In id elementum turpis.
-          Curabitur quis tincidunt mauris.
-        </p>
+      <div className='c-page-wrapper'>
+        <div className='c-content'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum
+            mauris, venenatis sit amet porttitor id, laoreet eu magna. In
+            convallis diam volutpat libero tincidunt semper. Ut aliquet erat
+            rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit
+            amet diam malesuada, eget laoreet quam molestie. In id elementum
+            turpis. Curabitur quis tincidunt mauris.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum
+            mauris, venenatis sit amet porttitor id, laoreet eu magna. In
+            convallis diam volutpat libero tincidunt semper. Ut aliquet erat
+            rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit
+            amet diam malesuada, eget laoreet quam molestie. In id elementum
+            turpis. Curabitur quis tincidunt mauris.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum
+            mauris, venenatis sit amet porttitor id, laoreet eu magna. In
+            convallis diam volutpat libero tincidunt semper. Ut aliquet erat
+            rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit
+            amet diam malesuada, eget laoreet quam molestie. In id elementum
+            turpis. Curabitur quis tincidunt mauris.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            consequat pretium turpis, in eleifend mi laoreet sed. Donec ipsum
+            mauris, venenatis sit amet porttitor id, laoreet eu magna. In
+            convallis diam volutpat libero tincidunt semper. Ut aliquet erat
+            rutrum, venenatis lacus ut, ornare lectus. Quisque congue ex sit
+            amet diam malesuada, eget laoreet quam molestie. In id elementum
+            turpis. Curabitur quis tincidunt mauris.
+          </p>
+        </div>
+        <div className='c-images'>
+          <div className='c-main-image'></div>
+          <div className='c-other-images'>
+            <div className='c-image-2'></div>
+            <div className='c-image-3'></div>
+          </div>
+        </div>
       </div>
-      <div className='c-images'></div>
     </div>
   );
 };
