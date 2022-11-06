78c8a448a7ae352efa2f8f945e2a7c419d19f12d
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-11-06 15:31:03 +0100

    Added some basic logic for handling exceptions on controller level

---
+++ auction-app-backend/src/main/java/com/internship/auctionapp/middleware/exception/RestExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.internship.auctionapp.middleware.exception;
+
+import org.hibernate.PropertyValueException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.NoSuchElementException;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice()
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(PropertyValueException.class)
+    public ResponseEntity<Object> handleSqlIntegrityException(HttpServletRequest req, PropertyValueException ex) {
+        String error = "Unable to submit post: " + ex.getMessage();
+        return buildResponseEntity(new ErrorResponse(HttpStatus.BAD_REQUEST, error));
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> handleNoSuchElementException(HttpServletRequest req, NoSuchElementException ex) {
+        String message = "The row for address is not existent: " + req.getRequestURI();
+        return buildResponseEntity(new ErrorResponse(HttpStatus.NOT_FOUND, message));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ErrorResponse errorResponse) {
+        return new ResponseEntity<Object>(errorResponse, errorResponse.getStatus());
+    }
+
+}
--- auction-app-backend/src/main/java/com/internship/auctionapp/models/Product.java
+++ auction-app-backend/src/main/java/com/internship/auctionapp/models/Product.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -21,24 +22,31 @@ public class Product {
     @Column(name = "Id", updatable = false, nullable = false)
     private UUID id;
 
+    @Column(nullable = false)
     @NotBlank(message = "Product Name is required")
     private String name;
 
+    @Column(nullable = false)
     @NotBlank(message = "Product Description is required")
     private String description;
 
+    @Column(nullable = false)
     @NotBlank(message = "Product ImageURL is required")
     private String imageURL;
 
+    @Column(nullable = false)
     @NotBlank(message = "Product Creation Date is required")
     private LocalDate creationDate;
 
+    @Column(nullable = false)
     @NotBlank(message = "Product Expiration Date is required")
     private LocalDate expirationDate;
 
+    @Column(nullable = false)
     @NotBlank(message = "Product Status is required")
     private String status;
 
+    @Column(nullable = false)
     @NotBlank(message = "Product Size is required")
     private String size;
 }
