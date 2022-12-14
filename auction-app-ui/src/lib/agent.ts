a804a9b153e08bac418e3f6be6a8824a09e6d207
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-12-14 12:59:03 +0100

    Added axios interceptor which takes token from localstorage and sets it in authorization header

--- auction-app-ui/src/lib/agent.ts
+++ auction-app-ui/src/lib/agent.ts
@@ -2,6 +2,14 @@ import axios, { AxiosResponse } from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:8080/api/v1/';
 
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+  if(token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config;
+})
+
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 // eslint-disable-next-line import/no-anonymous-default-export
