446186477654f751f8fb326b350535c44ffa99ee
Muamer Aličković <alickovicmuamer1@gmail.com>
2023-02-04 15:44:40 +0100

    Fixed problem with getting id from request

--- auction-app-backend/src/main/java/com/internship/auctionapp/controllers/CategoryController.java
+++ auction-app-backend/src/main/java/com/internship/auctionapp/controllers/CategoryController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,8 +42,7 @@ public class CategoryController {
 
     @DeleteMapping("/{id}")
     @SecurityRequirement(name = "Bearer Authentication")
-    public void deleteCategory(@RequestParam UUID id){
+    public void deleteCategory(@PathVariable("id") UUID id){
         categoryService.deleteCategory(id);
     }
-
 }
