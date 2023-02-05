41f4e551b6a063be3140144f6762fc9aba91a218
Muamer Aličković <75814679+mr-am2k@users.noreply.github.com>
2023-02-05 14:59:51 +0100

    Bug fixes (#20)
    * Fixed bug for bidding price, added validation when creating product
    
    * Fixed visual unresponsiveness on shop page
    
    * Solved bug for not refreshing data when user change to related product
    
    * Solved bug for not refreshing data when user change to related product
    
    * Fixed bug for not displaying message in a sold product button

--- auction-app-backend/src/main/java/com/internship/auctionapp/middleware/exception/RestExceptionHandler.java
+++ auction-app-backend/src/main/java/com/internship/auctionapp/middleware/exception/RestExceptionHandler.java
@@ -182,6 +182,26 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return buildResponseEntity(new ErrorResponse(HttpStatus.BAD_REQUEST, "There has been problem with your payment!"));
     }
 
+    @ExceptionHandler(ProductNegativeStartPriceException.class)
+    public ResponseEntity<Object> handleProductNegativeStartPriceException(HttpServletRequest req, ProductNegativeStartPriceException ex) {
+        return buildResponseEntity(new ErrorResponse(HttpStatus.BAD_REQUEST, "Product price can't be lower than 1$"));
+    }
+
+    @ExceptionHandler(ProductImagesMinimumException.class)
+    public ResponseEntity<Object> handleProductImagesMinimumException(HttpServletRequest req, ProductImagesMinimumException ex) {
+        return buildResponseEntity(new ErrorResponse(HttpStatus.BAD_REQUEST, "You need to upload at least 3 images!"));
+    }
+
+    @ExceptionHandler(ProductImagesMaximumException.class)
+    public ResponseEntity<Object> handleProductImagesMaximumException(HttpServletRequest req, ProductImagesMaximumException ex) {
+        return buildResponseEntity(new ErrorResponse(HttpStatus.BAD_REQUEST, "You can't upload more than 10 images"));
+    }
+
+    @ExceptionHandler(ProductCreationDateTimeException.class)
+    public ResponseEntity<Object> handleProductCreationDateTimeException(HttpServletRequest req, ProductCreationDateTimeException ex) {
+        return buildResponseEntity(new ErrorResponse(HttpStatus.BAD_REQUEST, "Creation date can't be in the past"));
+    }
+
     private ResponseEntity<Object> buildResponseEntity(ErrorResponse errorResponse) {
         return new ResponseEntity<Object>(errorResponse, errorResponse.getStatus());
     }
